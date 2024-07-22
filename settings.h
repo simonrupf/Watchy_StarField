561e57c1c64455630aea193dca09f667e75e611a
Oleg Girko <ol@infoserver.lv>
2024-07-22 02:17:17 +0100

    Update settings to newer version of Watchy library.
    Now latitude and longitude can be used instead of city ID to retrieve
    weather information.
    OpenWeaterMap API URL has been changed accordingly.
    
    Also, locations of fields in watchySettings changed, so using explicit
    field names to initialise them.
    
    Signed-off-by: Oleg Girko <ol@infoserver.lv>

--- settings.h
+++ settings.h
@@ -3,8 +3,18 @@
 
 //Weather Settings
 #define CITY_ID "1796231" //Shanghai Shi https://openweathermap.org/current#cityid
+
+//You can also use LAT,LON for your location instead of CITY_ID, but not both
+//#define LAT "121.466667" //Shanghai, Looked up on https://www.latlong.net/
+//#define LON "31.23333"
+
+#ifdef CITY_ID
+    #define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?id={cityID}&lang={lang}&units={units}&appid={apiKey}" //open weather api using city ID
+#else
+    #define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&lang={lang}&units={units}&appid={apiKey}" //open weather api using lat lon
+#endif
+
 #define OPENWEATHERMAP_APIKEY "0d1949854101140caad159dc9a23ca86" //use your own API key :)
-#define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?id=" //open weather api
 #define TEMP_UNIT "metric" //metric = Celsius , imperial = Fahrenheit
 #define TEMP_LANG "en"
 #define WEATHER_UPDATE_INTERVAL 30 //must be greater than 5, measured in minutes
@@ -14,15 +24,21 @@
 #define DST_OFFSET_SEC 3600
 
 watchySettings settings{
-    CITY_ID,
-    OPENWEATHERMAP_APIKEY,
-    OPENWEATHERMAP_URL,
-    TEMP_UNIT,
-    TEMP_LANG,
-    WEATHER_UPDATE_INTERVAL,
-    NTP_SERVER,
-    GMT_OFFSET_SEC,
-    DST_OFFSET_SEC
+    #ifdef CITY_ID
+        .cityID = CITY_ID,
+    #else
+        .cityID = "",
+        .lat = LAT,
+        .lon = LON,
+    #endif
+    .weatherAPIKey = OPENWEATHERMAP_APIKEY,
+    .weatherURL = OPENWEATHERMAP_URL,
+    .weatherUnit = TEMP_UNIT,
+    .weatherLang = TEMP_LANG,
+    .weatherUpdateInterval = WEATHER_UPDATE_INTERVAL,
+    .ntpServer = NTP_SERVER,
+    .gmtOffset = GMT_OFFSET_SEC,
+    .vibrateOClock = false,
 };
 
 #endif
